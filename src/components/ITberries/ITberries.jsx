2d67d8c8a8a14add5a24aa0619f830f342eb6bb5
Roman <kybik_rybik1994@mail.ru>
2022-05-13 15:05:44 +0300

    ItBerries

--- src/components/ITberries/ITberries.jsx
+++ src/components/ITberries/ITberries.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./ITberries.css";
+import background from "../../assets/img/ITberries/background.png";
 
-function ITberries(props) {
-  return <div></div>;
-}
+const ITberries = () => {
+  return (
+    <div className="container__itberries">
+      <div className="tittle__itberries">it berries</div>
+      <div className="subtext__itberries">
+        Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius
+        finibus. Sed ornare sit amet lorem sed viverra. In vel urna quis libero
+        viverra facilisis ut ac est. Morbi commodo, eros in dignissim tempus,
+        lacus odio rutrum augue, in semper sem magna quis tellus. Etiam enim
+        erat, suscipit eu semper a, dictum sit amet elit. Nunc egestas nisi eget
+        enim gravida facilisis. Pellentesque laoreet varius turpis vel pharetra.
+        Ut ante justo, consequat vitae elementum tempor, accumsan nec eros.
+      </div>
+      <div className="btn__position">
+        <div href="#readmore" className="btn__readmore">
+          read more
+        </div>
+      </div>
+
+      <img src={background} className="background" alt="" />
+    </div>
+  );
+};
 
 export default ITberries;
