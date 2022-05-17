5bb9aa3137af31170985d21e99b812f73ce1c777
Roman <kybik_rybik1994@mail.ru>
2022-05-17 20:25:50 +0300

    end maket

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,21 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import Services from "../Services/Services";
 import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="home container">
+    <div className="hero container">
       <header className="header">
         <Navbar />
       </header>
-      <div className="container__hero">
-        <h5 className="hello">Hi, i am</h5>
-        <h1 className="name">Roman Yukhlin</h1>
-        <h5 className="sub_name">Front-end Developer</h5>
-        <Services />
-        <div className="img__rectangle_hero"></div>
+      <div className="hero__who-i-am">
+        <div className="hero__name">
+          <h5>Hi, I am</h5>
+          <h1>Roman Yukhlin</h1>
+          <h5>Front-end Developer</h5>
+        </div>
+        <div className="hero__about"></div>
       </div>
     </div>
   );
