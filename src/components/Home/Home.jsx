1f053740c50499ec9047afe1dca9bd635053f300
Roman <kybik_rybik1994@mail.ru>
2022-05-18 23:05:58 +0300

    hero end

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,48 @@
 import React from "react";
 import "./Home.css";
+import separator from "../../assets/img/Home/separator/separatorWhite.png";
+import mail from "../../assets/img/Home/contacts/mail.svg";
+import github from "../../assets/img/Home/contacts/github.svg";
+import telegram from "../../assets/img/Home/contacts/telegram.svg";
 
 const Home = () => {
   return (
     <div className="hero container">
-      <div className="hero__who-i-am">
-        <div className="hero__name">
-          <h5>Hi, I am</h5>
-          <h1>Roman Yukhlin</h1>
-          <h5>Front-end Developer</h5>
+      <div className="hero__header">
+        <h1 className="hero__main">Roman Yukhlin</h1>
+        <h3 className="hero__submain">Front-end Developer</h3>
+        <img src={separator} className="hero__separator" alt="separator" />
+        <div className="hero__social-media">
+          <a href="malito:spiceandwolfroma@gmail.com">
+            <img
+              className="hero__social-media-unit"
+              src={mail}
+              alt="mail-svg"
+            />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/kybikchidori1"
+          >
+            <img
+              className="hero__social-media-unit"
+              src={github}
+              alt="git-svg"
+            />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://t.me/Kybikchidori1"
+          >
+            <img
+              className="hero__social-media-unit-tg"
+              src={telegram}
+              alt="telegram-svg"
+            />
+          </a>
         </div>
-        <div className="hero__about"></div>
       </div>
     </div>
   );
