585082656f183cadad1571c83bdb75b003835344
Roman <kybik_rybik1994@mail.ru>
2022-05-19 00:42:31 +0300

    skill end

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,99 +13,59 @@ import vscode from "../../assets/img/Skills/vscode.png";
 
 const Skills = () => {
   return (
-    <div className="skills container activation" id="skills">
-      <div className="skills__heading">
-        <h2>Skills</h2>
-        <p>I have exerience with</p>
-      </div>
-      <div className="skills__container">
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={react} className="skills__png" alt="" />
+    <div className="skills" id="skills">
+      <h1 className="sc-title gracia skills__header">SKILLS</h1>
+      <div className="skills__all">
+        <h1 className="skills__h1">USING NOW:</h1>
+        <div className="skills__container">
+          <div className="skill">
+            <img src={react} className="skill__image" alt="react" />
+            <h3 className="skill__h3">React</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">react</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={redux} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={redux} className="skill__image" alt="redux" />
+            <h3 className="skill__h3">Redux</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">redux</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={bem} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={bem} className="skill__image" alt="bem" />
+            <h3 className="skill__h3">BEM</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">bem</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={git} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={git} className="skill__image" alt="git" />
+            <h3 className="skill__h3">Git</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">git</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={figma} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={figma} className="skill__image" alt="figma" />
+            <h3 className="skill__h3">Figma</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">figma</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={css} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={css} className="skill__image" alt="css" />
+            <h3 className="skill__h3">CSS3</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">css</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={github} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={html} className="skill__image" alt="html5" />
+            <h3 className="skill__h3">HTML5</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">github</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={html} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={github} className="skill__image" alt="github" />
+            <h3 className="skill__h3">GitHub</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">html</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={javascript} className="skills__png" alt="" />
+          <div className="skill">
+            <img src={javascript} className="skill__image" alt="javascript" />
+            <h3 className="skill__h3">Javascript</h3>
           </div>
-          <div className="skills__name">
-            <div className="skills__png">javascript</div>
-          </div>
-        </div>
 
-        <div className="skills__box">
-          <div className="skills__logo">
-            <img src={vscode} className="skills__png" alt="" />
-          </div>
-          <div className="skills__name">
-            <div className="skills__png">vscode</div>
+          <div className="skill">
+            <img src={vscode} className="skills__png" alt="vscode" />
+            <h3 className="skill__h3">VS Code</h3>
           </div>
         </div>
       </div>
